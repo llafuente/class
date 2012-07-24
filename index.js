f8b411de4ed1cd16745b4759705b7f7be98f7b6b
llafuente <llafuente@noboxout.com>
2012-07-24 10:35:58 +0200

    feature: event machine

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = require("./lib/class.js");
 // sugar
 module.exports.Eventize = require("./lib/eventize.js").Eventize;
 module.exports.Events = require("./lib/events.js").Events;
+module.exports.EventMachine = require("./lib/eventmachine.js").EventMachine;
 module.exports.Sequence = require("./lib/sequence.js").Sequence;
 module.exports.Iterable = require("./lib/iterable.js").Iterable;
 module.exports.Animate = require("./lib/animate.js").Animate;
