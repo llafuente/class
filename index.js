a3a7ef4c036e6a3c69cedf846e06c477afba4628
llafuente <llafuente@noboxout.com>
2012-10-31 09:16:40 +0100

    refactoring and small speed up avoiding lookups

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// vanilla
-module.exports = require("./lib/class.js");
+(function (exports, browser) {
+    "use strict";
 
-// sugar
-module.exports.Eventize = require("./lib/eventize.js").Eventize;
-module.exports.Events = require("./lib/events.js").Events;
-module.exports.EventMachine = require("./lib/eventmachine.js").EventMachine;
-module.exports.Sequence = require("./lib/sequence.js").Sequence;
-module.exports.Iterable = require("./lib/iterable.js").Iterable;
-module.exports.Animate = require("./lib/animate.js").Animate;
+    // vanilla
+    module.exports = require("./lib/class.js");
+
+    // sugar
+    module.exports.Eventize = require("./lib/eventize.js").Eventize;
+    module.exports.Events = require("./lib/events.js").Events;
+    module.exports.EventMachine = require("./lib/eventmachine.js").EventMachine;
+    module.exports.Sequence = require("./lib/sequence.js").Sequence;
+    module.exports.Iterable = require("./lib/iterable.js").Iterable;
+    module.exports.Animate = require("./lib/animate.js").Animate;
+
+}(typeof module == "undefined" ? $ : module.exports, typeof module == "undefined"));
